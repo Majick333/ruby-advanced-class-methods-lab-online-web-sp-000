84eeca131953cf7fe5cae1966116785792aa6922
Ryan M <rmccauleymusic@gmail.com>
2019-11-12 23:16:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,9 +52,10 @@ class Song
   end
 
   def self.create_from_filename(name)
-    if self.name.includes?(".mp3")
-    @@all << self.new_from_filename
-    end
+    song = self.new
+    song.name = (name.split(" - ")[1].chomp(".mp3"))
+    song.artist_name = (name.split(" - ")[0])
+    @@all << song
   end
 
   
